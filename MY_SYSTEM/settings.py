df7a5848e53cfe8fd3c1255fdb8444bed2ad99e7
lazyPy <janopatundog@gmail.com>
2022-10-08 11:20:42 +0800

    .

--- MY_SYSTEM/settings.py
+++ MY_SYSTEM/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-#import django_heroku
+import django_heroku
 import dj_database_url
 from decouple import config
 from pathlib import Path
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-        'django.contrib.sessions',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'My_App',
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'MY_SYSTEM.urls'
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'My_App/static'
 ]
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -139,4 +139,4 @@ GOOGLE_API_KEY = 'AIzaSyCv2n4qpTGjTb3LEriFYvzRmcw0kIkI0FU'
 
 AUTH_USER_MODEL = 'My_App.User'
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
