28904d92fda5483d3560403fd8bbfa31c681e06b
lazyPy <janopatundog@gmail.com>
2022-11-12 19:24:49 +0800

    .

--- MY_SYSTEM/settings.py
+++ MY_SYSTEM/settings.py
@@ -154,6 +154,6 @@ AWS_STORAGE_BUCKET_NAME = 'bh-finder-bucket'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
