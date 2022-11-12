aa097b3f65010da64195fd38c5d6f75d8e27ab82
lazyPy <janopatundog@gmail.com>
2022-11-12 19:56:42 +0800

    .

--- My_App/models.py
+++ My_App/models.py
@@ -33,7 +33,7 @@ class BoardingHouse(models.Model):
         ordering = ['-updated', '-created']
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
 
 class Picture(models.Model):
